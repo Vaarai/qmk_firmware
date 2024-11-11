bd7bac3d2ee48dbd0a2390bb395378f968c03b17
Vaarai <joshwolfcarius@gmail.com>
2024-11-11 13:22:18 +0100

    Add Chocozor keyboard

---
+++ keyboards/chocozor/keymaps/fire/rules.mk
@@ -0,0 +1,9 @@
+SRC += tap_dances.c
+
+MOUSEKEY_ENABLE = yes
+CAPS_WORD_ENABLE = yes
+TAP_DANCE_ENABLE = yes
+
+# Cirque Trackpad I²C configuration
+POINTING_DEVICE_ENABLE = yes
+POINTING_DEVICE_DRIVER = cirque_pinnacle_i2c
